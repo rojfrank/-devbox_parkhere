06df79f3b1d9573efc2cb8923ac098cb44f4b2a3
Frank Rojas <frojas@grupotsiperu.com.pe>
2018-04-27 23:58:49 -0500

    modificaciones en el login de propietarios

--- app_devbox_parkhere/app/models/user.rb
+++ app_devbox_parkhere/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
     validates :No_Nombres, :presence => { message: "es requerido" },
     :length => { :minimum => 0, :too_short => "debe ser mayor a 0" }
+    
+    belongs_to :tipo_identidad
+    belongs_to :tipo_usuario
 end
--- app_devbox_parkhere/db/schema.rb
+++ app_devbox_parkhere/db/schema.rb
@@ -10,8 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20180427052437) do
+ActiveRecord::Schema.define(version: 20180428041438) do
+
+  create_table "tipo_identidads", force: :cascade do |t|
+    t.integer "co_identidad"
+    t.string "no_nombre"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tipo_usuarios", force: :cascade do |t|
+    t.integer "co_tipousuario"
+    t.string "no_nombre"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "No_Nombres"
@@ -25,19 +38,6 @@ ActiveRecord::Schema.define(version: 20180427052437) do
     t.string "No_Correo"
     t.string "Pw_Contrasenia"
     t.string "Co_TipoUsuario"
-=======
-ActiveRecord::Schema.define(version: 20180428041438) do
-
-  create_table "tipo_identidads", force: :cascade do |t|
-    t.integer "co_identidad"
-    t.string "no_nombre"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "tipo_usuarios", force: :cascade do |t|
-    t.integer "co_tipousuario"
-    t.string "no_nombre"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -55,7 +55,6 @@ ActiveRecord::Schema.define(version: 20180428041438) do
     t.string "no_celular"
     t.string "no_direccion"
     t.boolean "fl_situacion"
->>>>>>> 30dae8f66d2b20c66a558f564cc14e106dd06873
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
