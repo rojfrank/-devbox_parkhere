8f683880c3349ae8419b69db2aa388b72b4e4c34
Frank Rojas <frojas@grupotsiperu.com.pe>
2018-04-28 23:50:53 -0500

    modificación en login

--- app_devbox_parkhere/app/controllers/sessions_controller.rb
+++ app_devbox_parkhere/app/controllers/sessions_controller.rb
@@ -6,13 +6,13 @@ class SessionsController < ApplicationController
     def create
         user = User.find_by_email(params[:email])
         # If the user exists AND the password entered is correct.
-        if user!=nil and user.password_digest==params[:password]
+        if user!=nil and user.password_digest.equals(params[:password]) 
           # Save the user id inside the browser cookie. This is how we keep the user 
           # logged in when they navigate around our website.
           session[:user_id] = user.id
           redirect_to '/'
         else
-        # If user's login doesn't work, send them back to the login form.
+          #flash[:danger] = 'Invalid email/password combination' # Not quite right!
           redirect_to pages_index_login_path
         end
     end
