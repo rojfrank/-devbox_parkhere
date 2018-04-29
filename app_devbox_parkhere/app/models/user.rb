83fad54f86815f808f0251720c3c8583da9e37ab
jronaldcll <37959861+jronaldcll@users.noreply.github.com>
2018-04-29 00:30:20 -0500

    validaciones
    Por verificar

--- app_devbox_parkhere/app/models/user.rb
+++ app_devbox_parkhere/app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ApplicationRecord
     validates :No_Nombres, :presence => { message: "es requerido" },
-    :length => { :minimum => 1, :too_short => "debe ser mayor a 0" }
+    :length => { :minimum => 2, :too_short => "Nombre no válido" }
+
+    validates :No_ApellidoPaterno, :presence => { message: "es requerido" },
+    :length => { :minimum => 2, :too_short => "Apellido no válido" }
+
+    validates :No_ApellidoMaterno, :presence => { message: "es requerido" },
+    :length => { :minimum => 2, :too_short => "Apellido no válido" }
     
-  
+    validates :No_Celular, length: { is: 9 }
+    validates :No_Documento, length: { is: 8 }
+    validates :Pw_Contrasenia, length: { in: 6..20 }
+    validates :Fl_Sexo, :Fe_Nacimiento, absence: true
+    validates :No_Correo, uniqueness: true
+    validates :No_Documento, uniqueness: true
+
 end
