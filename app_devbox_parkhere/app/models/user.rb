3629484d49730036e6c625c4cdfe27bb251c8ad5
jronaldcll <37959861+jronaldcll@users.noreply.github.com>
2018-05-06 13:15:08 -0500

    validaciones
    comentando

--- app_devbox_parkhere/app/models/user.rb
+++ app_devbox_parkhere/app/models/user.rb
@@ -1,19 +1,19 @@
 class User < ApplicationRecord
         
-    validates :No_Nombres, :presence => { message: "es requerido" },
-    :length => { :minimum => 2, :too_short => "Nombre no válido" }
-
-    validates :No_ApellidoPaterno, :presence => { message: "es requerido" },
-    :length => { :minimum => 2, :too_short => "Apellido no válido" }
-
-    validates :No_ApellidoMaterno, :presence => { message: "es requerido" },
-    :length => { :minimum => 2, :too_short => "Apellido no válido" }
-    
-    validates :No_Celular, length: { is: 9 }
-    validates :No_Documento, length: { is: 8 }
-    validates :Pw_Contrasenia, length: { in: 6..20 }
-    validates :Fl_Sexo, :Fe_Nacimiento, absence: true
-    validates :No_Correo, uniqueness: true
-    validates :No_Documento, uniqueness: true
+#    validates :No_Nombres, :presence => { message: "es requerido" },
+#    :length => { :minimum => 2, :too_short => "Nombre no válido" }
+#
+#    validates :No_ApellidoPaterno, :presence => { message: "es requerido" },
+#    :length => { :minimum => 2, :too_short => "Apellido no válido" }
+#
+#    validates :No_ApellidoMaterno, :presence => { message: "es requerido" },
+#    :length => { :minimum => 2, :too_short => "Apellido no válido" }
+#    
+#    validates :No_Celular, length: { is: 9 }
+#    validates :No_Documento, length: { is: 8 }
+#    validates :Pw_Contrasenia, length: { in: 6..20 }
+#    validates :Fl_Sexo, :Fe_Nacimiento, absence: true
+#    validates :No_Correo, uniqueness: true
+#    validates :No_Documento, uniqueness: true
 
 end
